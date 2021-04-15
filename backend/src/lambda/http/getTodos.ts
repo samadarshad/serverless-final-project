0092ca3d404cc314d6698972b7a8e0755d262a79
samad <samadarshad@hotmail.co.uk>
2021-04-15 16:14:09 +0100

    Middy for cors

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,15 +2,28 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('getTodos')
+
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+    logger.info('Processing event', {
+        event
+    })
+  
   return {
     statusCode: 200,
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    },
     body: JSON.stringify({
         message: "getTodos endpoint"
     })
 }
-}
+})
+
+handler.use(
+    cors({
+        credentials: true
+    })
+)
