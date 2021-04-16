4d5468a1c435bab57c52b8e465339a8d0ded63ff
samad <samadarshad@hotmail.co.uk>
2021-04-16 14:42:50 +0100

    Auth Download Cert and IamRolesPerFunction

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,23 +1,26 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { APIGatewayAuthorizerEvent, APIGatewayAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
+const jwksClient = require('jwks-rsa');
+
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://samadarshad.eu.auth0.com/.well-known/jwks.json'
 
+const keyClient = jwksClient({
+  jwksUri: jwksUrl
+})
+
 export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
+  event: APIGatewayAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  logger.info('Authorizing a user', { authorizationToken: event.authorizationToken })
+  
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
@@ -58,10 +61,10 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const key = await keyClient.getSigningKey(jwt.header.kid)
+  const certificate = key.getPublicKey()
+
+  return verify(token, certificate) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
