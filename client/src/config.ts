415bde7d64697e2dd301783afcfa899280344ea3
samad <samadarshad@hotmail.co.uk>
2021-04-20 17:20:42 +0100

    Add URL to client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 
-export const apiEndpoint = `http://localhost:3003/dev`
-
+export const apiEndpoint = `https://vrcrzzx032.execute-api.eu-west-2.amazonaws.com/dev` 
+// export const apiEndpoint = `http://localhost:3003/dev`
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'samadarshad.eu.auth0.com',            // Auth0 domain
