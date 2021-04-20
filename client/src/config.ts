872f20399f133c68d1d9b169172038f3dfc78168
samad <samadarshad@hotmail.co.uk>
2021-04-20 18:18:28 +0100

    netlify callback url for auth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'samadarshad.eu.auth0.com',            // Auth0 domain
   clientId: 'CAuysQoEuuxWV4Ui4zayLHSjWFBVsGUb',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  // callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://serverless-project-samadarshad.netlify.app/callback'
 }
